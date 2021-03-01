94f00ae9754caa7d18f462e48670a757c02f964d
Danilaboni <72703073+danilaboni@users.noreply.github.com>
2021-03-02 02:48:11 +0300

    new alpha version

--- csv_converter/scw2csv.py
+++ csv_converter/scw2csv.py
@@ -16,35 +16,27 @@ head_mat_name = struct.unpack(f'>{head_mat_length}s', sf.read(head_mat_length))[
 head_unkBool= struct.unpack('?', sf.read(1))[0]
 head_crc32 = struct.unpack('>I', sf.read(4))[0]
 
-geom_uint32_length = struct.unpack('>I', sf.read(4))[0]
-geom = struct.unpack('4s', sf.read(4))[0].decode()
-geom_name_length = struct.unpack('>h', sf.read(2))[0]
-geom_name = struct.unpack(f'>{geom_name_length}s', sf.read(geom_name_length))[0].decode()
-geom_group_length = struct.unpack('>h', sf.read(2))[0]
-geom_group = struct.unpack(f'>{geom_group_length}s', sf.read(geom_group_length))[0].decode()
-geom_vertices_count = struct.unpack('B', sf.read(1))[0]
-
 if sc3d != 'SC3D':
     print('SC3D Not Found')
     sys.exit()
 
-elif geom == 'MATE':
-    print('Mate Unsupported')
-    sys.exit()
-
 save_parsed = open('converted.csv', 'w')
-save_parsed.write(f'Signatures,,\n')
-save_parsed.write(f'{sc3d},File Format\n')
+
+save_parsed.write(f'Signatures,Info1,Info2\n')
+
+save_parsed.write(f'Sc3d,File Format,{sc3d}\n')
+
 save_parsed.write(f'Head,Head Length,{head_uint32_length}\n')
+save_parsed.write(f'Head,Type,{head}\n')
 save_parsed.write(f'Head,SCW Version,{head_version}\n')
+save_parsed.write(f'Head,Frame Rate,{head_frameRate}\n')
 save_parsed.write(f'Head,Unknown 2,{head_unk2}\n')
 save_parsed.write(f'Head,Anim Frame End,{head_AnimFrameEnd}\n')
 save_parsed.write(f'Head,Material Length,{head_mat_length}\n')
 save_parsed.write(f'Head,Material Path,{head_mat_name}\n')
 save_parsed.write(f'Head,Unknown B,{head_unkBool}\n')
 save_parsed.write(f'Head,Crc32,{head_crc32}\n')
-save_parsed.write(f'Geom,Geom Length,{geom_uint32_length}\n')
-save_parsed.write(f'Geom,Name,{geom_name}\n')
-save_parsed.write(f'Geom,Group,{geom_group}\n')
-save_parsed.write(f'Geom,Vertices Count,{geom_vertices_count}')
+
+print('Successfully!')
+
 save_parsed.close()
